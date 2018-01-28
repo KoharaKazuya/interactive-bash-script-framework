f16e8da9325c6486fd1a578cc5b9524623742064
KoharaKazuya <reflect.results@gmail.com>
2018-01-29 00:21:58 +0900

    add comment

--- ibsf.sh
+++ ibsf.sh
@@ -1,8 +1,17 @@
+#
+# Interactive Bash Script Framework
+#
+# Define ibsf_* functions.
+
+# Trap errors
+#
+# Operation script must call this as soon as possible.
 ibsf_begin_trap() {
   set -E
   trap 'ibsf_onerror' ERR
   trap 'ibsf_oninterapt' INT
 }
+# @private
 ibsf_onerror() {
   print_fail '処理に失敗したため、スクリプトの実行を中断します。'
   printf '\n\n%s' "$_ibsf_deco_weak"
@@ -10,12 +19,16 @@ ibsf_onerror() {
   printf '  Line: %s\n' "${BASH_LINENO[0]}"
   printf '%s\n' "$_ibsf_deco_reset"
 }
+# @private
 ibsf_oninterapt() {
   trap INT
   print_info '中断が要求されました。'
   printf '\n\n'
 }
 
+# Show message and confirmation
+#
+# @param $* messages
 ibsf_message() {
   print_separator
   print_message "$*"
@@ -24,6 +37,12 @@ ibsf_message() {
   read
 }
 
+# Confirm execution of external script
+#
+# Show content of external script, and confirm before execution.
+#
+# @param $1 action file path
+# @param $2 action friendly name instead of action file path (optional)
 ibsf_exec_action() {
   [ $# -gt 0 ]
   local action_file="$1"
@@ -78,6 +97,7 @@ ibsf_exec_action() {
   ibsf_exec_action "$@"
 }
 
+# @private standard output decorations
 _ibsf_deco_reset="$(     tput sgr0     2>/dev/null || : )"
 _ibsf_deco_underline="$( tput smul     2>/dev/null || : )"
 _ibsf_deco_bold="$(      tput bold     2>/dev/null || : )"
@@ -89,29 +109,53 @@ _ibsf_deco_yellow="$(    tput setaf 3  2>/dev/null || : )"
 _ibsf_deco_cyan="$(      tput setaf 6  2>/dev/null || : )"
 _ibsf_deco_white="$(     tput setaf 7  2>/dev/null || : )"
 
+# Show line sparator
 print_separator() {
   printf '%s%s%s' "$_ibsf_deco_reset" "$_ibsf_deco_underline" "$_ibsf_deco_weak"
   printf "%$( tput cols 2>/dev/null || : )s"
   printf '%s\n\n' "$_ibsf_deco_reset"
 }
+# Show message with indent
+#
+# @param $* message
 print_message() {
   printf '%s       %s%s%s' "$_ibsf_deco_reset" "$_ibsf_deco_bold" "$*" "$_ibsf_deco_reset"
 }
+# Show message with label
+#
+# @param $1 color of message (one of _ibsf_deco_* colors)
+# @param $2 string of label (must be 4 characters)
+# @param $3 message
 print_label() {
   printf '%s%s%s %s %s %s%s%s' "$_ibsf_deco_reset" "$1" "$_ibsf_deco_rev" "$2" "$_ibsf_deco_reset" "$1" "$3" "$_ibsf_deco_reset"
 }
+# Show message with 'OK' label
+#
+# @param $* message
 print_ok() {
   print_label "$_ibsf_deco_green" ' OK ' "$*"
 }
+# Show message with 'ASK' label
+#
+# @param $* message
 print_ask() {
   print_label "$_ibsf_deco_cyan" ' ASK' "$*"
 }
+# Show message with 'INFO' label
+#
+# @param $* message
 print_info() {
   print_label "$_ibsf_deco_white" 'INFO' "$*"
 }
+# Show message with 'WARN' label
+#
+# @param $* message
 print_warn() {
   print_label "$_ibsf_deco_yellow" 'WARN' "$*"
 }
+# Show message with 'FAIL' label
+#
+# @param $* message
 print_fail() {
   print_label "$_ibsf_deco_red" 'FAIL' "$*"
 }
